2ac329a735984e1eab9e3216982e2d655ffa9233
LucasArrabal <lucas.arrabal2003@gmail.com>
2024-01-29 09:40:06 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React , {Component} from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+
+    }
+  }
+
+
+
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <View style={styles.areaTitulo}>
+        <Text  style={styles.Titulo}>Cadastro da Conta</Text>
+        </View>
+
+        <TextInput
+        style={styles.nomeInput}
+        placeholder='Digite seu nome completo'
+        underlineColorAndroid="transparent"
+        />
+
+        
+      </View>
+    );
+  }
 }
 
+export default App;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  areaTitulo: {
+    margin: 10,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  Titulo: {
+    fontSize: 30
+  },
+  nomeInput: {
+    height: 45,
+    borderWidth: 2,
+    borderRadius: 5,
+    borderColor: 'black',
+    margin: 10,
+    fontSize: 20,
+    padding: 10
+  }
+
 });
